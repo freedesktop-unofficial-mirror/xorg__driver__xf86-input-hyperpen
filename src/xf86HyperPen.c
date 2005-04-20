5a3788c91af6f9eec75fbce0b87d0f8a56215ec2
Daniel Stone <daniel@fooishbar.org>
2005-04-20 12:25:33 +0000

    Fix includes right throughout the Xserver tree: change "foo.h" to <X11/foo.h> for core headers, e.g. X.h, Xpoll.h; change "foo.h", "extensions/foo.h" and "X11/foo.h" to     <X11/extensions/foo.h> for extension headers, e.g. Xv.h; change "foo.[ch]" to <X11/Xtrans/foo.[ch]> for Xtrans files.

--- src/xf86HyperPen.c
+++ src/xf86HyperPen.c
@@ -104,18 +104,18 @@ static InputDriverPtr hypDrv;
 
 #else  /* pre 3.9 headers */
 
-#include "Xos.h"
+#include <X11/Xos.h>
 #include <signal.h>
 #include <stdio.h>
 
 #define NEED_EVENTS
-#include "X.h"
-#include "Xproto.h"
+#include <X11/X.h>
+#include <X11/Xproto.h>
 #include "misc.h"
 #include "inputstr.h"
 #include "scrnintstr.h"
-#include "XI.h"
-#include "XIproto.h"
+#include <X11/extensions/XI.h>
+#include <X11/extensions/XIproto.h>
 
 #if defined(sun) && !defined(i386)
 #define POSIX_TTY
